fe32671c9c627963b6e74c8ef54fdceddf3215ef
gracenarez333 <gracemnarez@gmail.com>
2022-05-09 10:52:18 -0700

    adds a function to animate player

--- js/java.js
+++ js/java.js
@@ -11,35 +11,48 @@ const ctx = canvas.getContext('2d')
 canvas.setAttribute('height', getComputedStyle(canvas)['height'])
 canvas.setAttribute('width', getComputedStyle(canvas)['width'])
 // console.log(canvas)
-
+// console.log(canvas.height)
+// console.log(canvas.width)
 // CLASSES
 // controls creation of characters
 class Pawns {
-    constructor(x, y, color, width, height) {
+    constructor(x, y, color, width, height, velocity) {
         this.x = x
         this.y = y
         this.color = color
         this.width = width
         this.height = height
+        this.velocity = {
+            x: 0,
+            y: 20
+        }
         // this.alive = true;
     }
-
     render() {
         ctx.fillStyle = this.color
         ctx.fillRect(this.x, this.y, this.width, this.height)
     }
+    update() {
+        this.y += this.velocity.y
+        this.render()
+    }
 }
 // tester pawn
-const me = new Pawns(5, 5, 'blue', 30, 30)
-me.render()
-console.log(me)
+const mrRat = new Pawns(5, 5, 'purple', 30, 30)
+// me.update()
+// console.log(me)
 // GAME OBJECTS
 
 // GAME FUNCTIONS
-
+// draws the pawns
 function draw(x, y, width, height, color) {
     ctx.fillStyle = color
     ctx.fillRect(x, y, width, height)
 }
-
+// animates/updates screen for animation
+function animate() {
+    requestAnimationFrame(animate)
+    mrRat.update()
+}
+animate()
 // EVENT LISTENERS
