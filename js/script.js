d3e79f084eb2ee7f9ee50ab918932a09bf08fe4d
gracenarez333 <gracemnarez@gmail.com>
2022-05-13 08:27:00 -0700

    this cleans up code

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
-// SPRITE IMPORTS
-// import mrrat from '../img/mrrat.png' 
-// import slimey from '../img/slimey.png'
 // DOM SELECTORS
+
 // canvas element
 const canvas = document.querySelector('#canvas')
 // status display
@@ -13,24 +11,28 @@ const startBtn = document.querySelector('#startButton')
 
 // CANVAS SETUP / GAME STATE
 // set the rendering context of the canvas
+
 const ctx = canvas.getContext('2d')
 
 // set canvas width and height
+
 canvas.width = 800
 canvas.height = 400
-// console.log(canvas)
-// console.log(canvas.height)
-// console.log(canvas.width)
+
 // CLASSES
+
+let gameStartStatus = false
 let gameOverStatus = false
 
 const gameStatusTextElement = document.querySelector('[data-game-over-text]')
 const gameStatusElement = document.getElementById('gameOver')
 const startGameElement = document.getElementById('gameStart')
+
 // controls gravity of all pawns
 const gravity = .4
-// const bounce = 1
+
 // controls creation of characters
+// controls sprites
 const ratImage = new Image()
 ratImage.src = './img/mrrat.png'
 const slimeImage = new Image()
@@ -53,8 +55,6 @@ class Pawns {
     }
     // renders pawns on screen
     render() {
-        // ctx.fillStyle = this.color
-        // ctx.fillRect(this.x, this.y, this.width, this.height)
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
     }
     // this updates the position as the player is drawn
@@ -75,13 +75,11 @@ class Pawns {
 
         if(this.y + this.height + this.velocity.y >= canvas.height){
         this.velocity.y = -this.velocity.y
-        // this.bounce = -this.bounce
         }
-    // } else this.bounce = 1
-}
+    }
 }
 // GAME OBJECTS
-// tester pawn
+// rat pawns
 let mrRat = new Pawns(30, 5, 'purple', 50, 60, 'player', ratImage)
 // key press tracker for mrRat movements
 const keys = {
@@ -94,18 +92,15 @@ const keys = {
 }
 // slimeballs
 let slimeballOne = new Pawns(180, 5, 'limegreen', 40, 50, 'slimeball', slimeImage)
-let slimeballTwo = new Pawns(350, 5, 'limegreen', 40, 50, 'slimeball', slimeImage)
+let slimeballTwo = new Pawns(350, -10, 'limegreen', 40, 50, 'slimeball', slimeImage)
 let slimeballThree = new Pawns(500, 5, 'limegreen', 40, 50, 'slimeball', slimeImage)
-let slimeballFour = new Pawns(650, 5, 'limegreen', 40, 50, 'slimeball', slimeImage)
+let slimeballFour = new Pawns(650, -10, 'limegreen', 40, 50, 'slimeball', slimeImage)
 // const slimeballFive = new Pawns(750, 5, 'limegreen', 50, 50, 'slimeball', slimeImage)
-// const slimeballSix = new Pawns(700, 5, 'limegreen', 50, 50, 'slimeball', slimeImage)
+// const slimeballSix = new Pawns(700, -10, 'limegreen', 50, 50, 'slimeball', slimeImage)
 const slimeballs = (Pawns.type === 'slimeball')
-// console.log(slimeballs)
-
-// me.update()
-// console.log(me)
 
 // GAME FUNCTIONS
+
 // draws the pawns
 function draw(x, y, width, height, color) {
     ctx.fillStyle = color
@@ -114,96 +109,79 @@ function draw(x, y, width, height, color) {
 // animates/updates screen for animation
 function animate() {
     if (gameOverStatus) {
-        // console.log('smd'); 
         return;
     }
-    requestAnimationFrame(animate)
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    mrRat.update()
-    winDetect()
+        requestAnimationFrame(animate)
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        mrRat.update()
+        winDetect()
     // hit detection for each slimeball
-    hitDetect(slimeballOne, mrRat)
-    hitDetect(slimeballTwo, mrRat)
-    hitDetect(slimeballThree, mrRat)
-    hitDetect(slimeballFour, mrRat)
+        hitDetect(slimeballOne, mrRat)
+        hitDetect(slimeballTwo, mrRat)
+        hitDetect(slimeballThree, mrRat)
+        hitDetect(slimeballFour, mrRat)
     // hitDetect(slimeballFive, mrRat)
     // hitDetect(slimeballSix, mrRat)
     // slimeball bounce
-    slimeballOne.slimeBounce()
-    slimeballTwo.slimeBounce()
-    slimeballThree.slimeBounce()
-    slimeballFour.slimeBounce()
+        slimeballOne.slimeBounce()
+        slimeballTwo.slimeBounce()
+        slimeballThree.slimeBounce()
+        slimeballFour.slimeBounce()
     // slimeballFive.slimeBounce()
     // slimeballSix.slimeBounce()
-    // slimeballs.update()
-    slimeballOne.update()
-    slimeballTwo.update()
-    slimeballThree.update()
-    slimeballFour.update()
+    // slimeballs update
+        slimeballOne.update()
+        slimeballTwo.update()
+        slimeballThree.update()
+        slimeballFour.update()
     // slimeballFive.update()
     // slimeballSix.update()
 // if statement for rat movement
-    if (keys.right.pressed && mrRat.x < 800) {
-        mrRat.velocity.x = 2.7
-    } else if (keys.left.pressed && mrRat.x > 30) {
-        mrRat.velocity.x = -2.7
-    } else mrRat.velocity.x = 0
+            if (keys.right.pressed && mrRat.x < 800) {
+            mrRat.velocity.x = 2.7
+            } else if (keys.left.pressed && mrRat.x > 30) {
+            mrRat.velocity.x = -2.7
+            } else mrRat.velocity.x = 0
 }
-animate()
+
 // EVENT LISTENERS
 // handles movement
 // switch statement event listener for key down events
 addEventListener('keydown', ({key}) => {
     switch (key) {
         case('ArrowRight'):
-            // console.log('right')
-            // mrRat.velocity.x += 1
             keys.right.pressed = true
             break
         
         case('ArrowLeft'):
-            // console.log('left')
-            // mrRat.velocity.x -= 1
             keys.left.pressed = true
             break
 
         case('ArrowUp'):
-            // console.log('up')
             mrRat.velocity.y -= 5
             break
         
         case('ArrowDown'):
-            // console.log('down')
             mrRat.velocity.y = 0
             break
     }
-    // console.log(keys.right.pressed)
 })
+
 // switch statement event listener for key up events for smooth movement across keyboard settings
 addEventListener('keyup', ({key}) => {
     switch (key) {
         case('ArrowRight'):
-            // console.log('right')
-            // mrRat.velocity.x = 0
             keys.right.pressed = false
             break
         
         case('ArrowLeft'):
-            // console.log('left')
-            // mrRat.velocity.x = 0
             keys.left.pressed = false
             break
 
         case('ArrowUp'):
-            // console.log('up')
             mrRat.velocity.y -= 5
             break
-        
-        // case('ArrowDown'):
-        //     // console.log('down')
-        //     break
     }
-    // console.log(keys.right.pressed)
 })
 
 
@@ -220,48 +198,43 @@ function hitDetect(slimeball, player) {
         player.y <= slimeball.y + slimeball.height
     ) {
         mrRat.alive = false
-        // console.log('hit detected')
     }
 }
+
 // win detect function 
 function winDetect() {
     if (mrRat.alive === true && mrRat.x >= 800) {
-        // console.log('you win')
-        // ctx.clearRect(0, 0, canvas.width, canvas.height)
         gameOverStatus = true
         gameStatusTextElement.innerText = 'you win!'
         gameStatusElement.classList.add('show')
-        // animate()
     } else if (mrRat.alive === false && mrRat.x < 800) {
-        // console.log('you lose')
-        // ctx.clearRect(0, 0, canvas.width, canvas.height)
         gameOverStatus = true
         gameStatusTextElement.innerText = 'you lose!'
         gameStatusElement.classList.add('show')
-        // animate()
     } 
 }
 
+// start button event listener
 startBtn.addEventListener('click', function(){
     go()
 })
-
+// go function to start rendering when start is clicked
 function go() {
     startGameElement.classList.add('hidden')
+    animate()
 }
-
+// replay button event listener
 replayBtn.addEventListener('click', function(){
     refresh()
 })
-
+// refresh function to clear game and re-render
 function refresh() {
     gameOverStatus = false
     mrRat = new Pawns(30, 5, 'purple', 50, 60, 'player', ratImage)
     slimeballOne = new Pawns(180, 5, 'limegreen', 40, 50, 'slimeball', slimeImage)
-    slimeballTwo = new Pawns(350, 5, 'limegreen', 40, 50, 'slimeball', slimeImage)
+    slimeballTwo = new Pawns(350, -10, 'limegreen', 40, 50, 'slimeball', slimeImage)
     slimeballThree = new Pawns(500, 5, 'limegreen', 40, 50, 'slimeball', slimeImage)
-    slimeballFour = new Pawns(650, 5, 'limegreen', 40, 50, 'slimeball', slimeImage)
+    slimeballFour = new Pawns(650, -10, 'limegreen', 40, 50, 'slimeball', slimeImage)
     gameStatusElement.classList.remove('show')
     animate()
-    // console.log('smd again')
 }
